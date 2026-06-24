52bcdd8ce0d1bed22e9cf3f624a36d92fb6bb2ce
MiguelEscolaMaluf <miguel.iriguchi@escola.pr.gov.br>
2026-06-24 12:35:11 -0300

    if(todos)

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const Input = document.getElementsByClassName ('input')
 const todoUl = document.getElementById ('todo')
 
 const todos = JSON.parse(localStorage.getItem('todos'))
+
+//if é legal e daora
+if(todos){
+    todos.forEach(todo => addTodo(todo))}
